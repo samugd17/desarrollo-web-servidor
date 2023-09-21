a20f437988abbba9b11e7714d8cfc596a1533ec4
samugd17 <samugd17@gmail.com>
2023-09-21 15:01:33 +0100

    Se añade versión recursiva

--- max_product.py
+++ max_product.py
@@ -17,13 +17,26 @@ def max_product(sequence: str, span: int) -> int:
             max_result = result
     return max_result
 
-# def max_product(number: str, span: int, i: int, max_res: int):
-#     res = 1
-#     for n in number[i:i+span]:
-#         res *= int(n)
-#     if res > max_res:
-#         max_res = res
-#     i += 1
-#     return max_product(number, span, i, max_res) if (len(number) - i >= span) else max_res
+# VERSIÓN RECURSIVA
 
-# print(max_product(number, span, 0, 0))
+# def max_product(sequence: str, span: int) -> int:
+#     try:
+#         int(sequence)
+#     except:
+#         raise ValueError("El primer argumento ha de ser un número entero") 
+#     if span > len(sequence):
+#         raise ValueError("El segundo argumento no puede ser mayor que el primero")
+#     if span <= 0:
+#         raise ValueError("El segundo argumento no puede ser menor o igual a 0")    
+#     candidates = []
+#     def calc_candidates(sequence:str, span: int):
+#         candidates.append(calc_product(sequence[:span]))
+#         if len(sequence) - span == 0:
+#             return None
+#         return calc_candidates(sequence[1:], span)  
+#     def calc_product(sequence: str) -> int:
+#         if len(sequence) > 1:
+#             return int(sequence[0]) * calc_product(sequence[1:])
+#         return int(sequence[0])
+#     calc_candidates(sequence, span)
+#     return max(candidates)
